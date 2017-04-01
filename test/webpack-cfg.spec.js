443af2324bd141580df7a8effb6b01b475c3b14e
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-01 19:59:12 -0300

    test: bundler

--- test/webpack-cfg.spec.js
+++ test/webpack-cfg.spec.js
@@ -1,5 +1,24 @@
 import test from 'ava-spec';
 import webpackCfg from '../';
 
+const bundler = webpackCfg({
+	client: './configs/client-*.js',
+	styles: './configs/styles-*.js',
+	server: './configs/server-*.js',
+});
+
 test('tools/webpackCfg', t => {
+	t.is(toString.call(bundler.registerTasks), '[object Function]', 'registerTasks method exists');
+	t.is(toString.call(bundler.setConfig), '[object Function]', 'setConfig method exists');
+	t.is(toString.call(bundler.common), '[object Object]', 'common property exists');
+	t.truthy(bundler.common.cfg && bundler.common.exe, 'common is a dotcfg');
+});
+
+test('tools/webpackCfg', t => {
+	const getConfig = bundler.setConfig((common, styles, server) => {
+		t.truthy(common.cfg && common.exe, 'common is a dotcfg');
+		t.truthy(styles.cfg && styles.exe, 'styles is a dotcfg');
+		t.truthy(server.cfg && server.exe, 'server is a dotcfg');
+	});
+	t.truthy(toString.call(getConfig), '[object Function]');
 });
