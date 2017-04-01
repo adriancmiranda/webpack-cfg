905e47e62df075e5b5f86e6b1d33170ecc50993c
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-01 16:37:56 -0300

    test: merge-entries should validate the format

--- test/merge-entries.spec.js
+++ test/merge-entries.spec.js
@@ -1,7 +1,7 @@
 import test from 'ava-spec';
 import { mergeEntries } from '../tools';
 
-test('tools/mergeEntries', t => {
+test('tools/mergeEntries should work', t => {
   const pugEntry = './source/index.pug';
   const txtEntry = ['./source/index.txt'];
   const jsonEntry = {
@@ -20,4 +20,16 @@ test('tools/mergeEntries', t => {
   t.truthy(entry.index.length === 3, 'You got two "index" entries.');
   t.truthy(entry.stats.length === jsonEntry.stats.length, 'You got one "stats" entry.');
   t.truthy(entry.main.length === 3, 'You got three "main" entries.');
+});
+
+test('tools/mergeEntries should validate', t => {
+  const wrongEntry = {
+    index: {
+      file: 'wrong-entry.js',
+    },
+  };
+  const error = t.throws(() => {
+    mergeEntries('foo.js', wrongEntry);
+  }, TypeError);
+  t.is(error.message, 'An entry has a invalid type.');
 });
