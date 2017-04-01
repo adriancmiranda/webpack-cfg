9d92038bf2f75f1a9958408900a33c6c4cdf7c03
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-01 20:05:35 -0300

    test: bundler

--- test/webpack-cfg.spec.js
+++ test/webpack-cfg.spec.js
@@ -14,11 +14,31 @@ test('tools/webpackCfg', t => {
 	t.truthy(bundler.common.cfg && bundler.common.exe, 'common is a dotcfg');
 });
 
-test('tools/webpackCfg', t => {
+test('tools/webpackCfg config function', t => {
 	const getConfig = bundler.setConfig((common, styles, server) => {
 		t.truthy(common.cfg && common.exe, 'common is a dotcfg');
 		t.truthy(styles.cfg && styles.exe, 'styles is a dotcfg');
 		t.truthy(server.cfg && server.exe, 'server is a dotcfg');
 	});
-	t.truthy(toString.call(getConfig), '[object Function]');
+	t.is(toString.call(getConfig), '[object Function]');
+});
+
+test('tools/webpackCfg config', t => {
+	const argv = { dev: true, run: ['client-watch'] };
+	const getConfig = bundler.setConfig(argv, (common, styles, server) => {
+		t.truthy(common.cfg && common.exe, 'common is a dotcfg');
+		t.truthy(styles.cfg && styles.exe, 'styles is a dotcfg');
+		t.truthy(server.cfg && server.exe, 'server is a dotcfg');
+	});
+	// t.is(toString.call(getConfig), '[object Object]');
+});
+
+test('tools/webpackCfg multi config', t => {
+	const argv = { dev: true, run: ['client-watch', 'server-watch'] };
+	const getConfig = bundler.setConfig(argv, (common, styles, server) => {
+		t.truthy(common.cfg && common.exe, 'common is a dotcfg');
+		t.truthy(styles.cfg && styles.exe, 'styles is a dotcfg');
+		t.truthy(server.cfg && server.exe, 'server is a dotcfg');
+	});
+	// t.truthy(Array.isArray(getConfig), '');
 });
