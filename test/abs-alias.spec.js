d14ed44f6693d52485d02495283a6bd64990cc17
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-02 10:16:10 -0300

    test: aliases

--- test/abs-alias.spec.js
+++ test/abs-alias.spec.js
@@ -2,4 +2,10 @@ import test from 'ava-spec';
 import { absAlias } from '../tools';
 
 test('tools/absAlias', t => {
+	const src = absAlias(process.cwd());
+	t.is(toString.call(src), '[object Function]');
+	t.is(src(), process.cwd());
+	t.is(src('index.js'), `${process.cwd()}/index.js`);
+	t.is(src('core', 'index.js'), `${process.cwd()}/core/index.js`);
+	t.is(src('core', 'facade', 'index.js'), `${process.cwd()}/core/facade/index.js`);
 });
--- test/alias.spec.js
+++ test/alias.spec.js
@@ -2,4 +2,10 @@ import test from 'ava-spec';
 import { alias } from '../tools';
 
 test('tools/alias', t => {
+	const src = alias('./source');
+	t.is(toString.call(src), '[object Function]');
+	t.is(src(), 'source');
+	t.is(src('index.js'), 'source/index.js');
+	t.is(src('core', 'index.js'), 'source/core/index.js');
+	t.is(src('core', 'facade', 'index.js'), 'source/core/facade/index.js');
 });
