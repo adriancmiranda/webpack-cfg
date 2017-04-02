a25e915b7911080e6968aec920215fd24424ab39
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-02 09:17:44 -0300

    test: parse-argv

--- test/parse-argv.spec.js
+++ test/parse-argv.spec.js
@@ -1,5 +1,18 @@
 import test from 'ava-spec';
 import { parseArgv } from '../tools';
 
-test('tools/parseArgv', t => {
+test('tools/parseArgv params', t => {
+	t.is(parseArgv.params(1), 1);
+	t.is(parseArgv.params('foo'), 'foo');
+	t.deepEqual(parseArgv.params(null), null);
+	t.deepEqual(parseArgv.params(undefined), {});
+	t.deepEqual(parseArgv.params({ run: 'client-watch,server-watch' }), {
+		run: ['client-watch', 'server-watch']
+	});
+	t.deepEqual(parseArgv.params({ dev:true, run: 'client-watch,server-watch' }), {
+		run: ['client-watch', 'server-watch'], dev: true
+	});
+	t.deepEqual(parseArgv.params({ run: 'client-watch' }), {
+		run: ['client-watch']
+	});
 });
