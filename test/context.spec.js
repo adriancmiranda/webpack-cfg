d71871b0cc3f8a936f041cd8e2f2a19e67a6a155
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-02 10:26:51 -0300

    test: context

--- test/context.spec.js
+++ test/context.spec.js
@@ -2,4 +2,12 @@ import test from 'ava-spec';
 import { context } from '../tools';
 
 test('tools/context', t => {
+	const ctx = context(process.cwd());
+	const src = context('source');
+	t.is(toString.call(ctx), '[object Function]');
+	t.is(toString.call(src), '[object Function]');
+	t.is(ctx(), process.cwd());
+	t.is(src(), 'source');
+	t.is(ctx('test'), './test');
+	t.is(src('foo'), './../foo');
 });
