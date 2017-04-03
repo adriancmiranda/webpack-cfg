5b0ac1002554062007d351f9805feeddb7caa041
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-02 22:17:03 -0300

    test: base-strategy

--- test/base-strategy.spec.js
+++ test/base-strategy.spec.js
@@ -1,10 +1,10 @@
 import test from 'ava-spec';
 import strategy from '../lib/templates/base-strategy';
 
-const fromFilename = '[name].js';
-const toFilename = '[name].[chunkhash].js';
-function getPristineObject() {
-	return {
+test('templates/baseStrategy', t => {
+	const fromFilename = '[name].js';
+	const toFilename = '[name].[chunkhash].js';
+	let from = {
 		output: {
 			filename: fromFilename
 		},
@@ -12,20 +12,12 @@ function getPristineObject() {
 			modules: ['node_modules']
 		}
 	};
-}
-
-test('templates/baseStrategy', t => {
-	// let from = getPristineObject();
-	// t.not(strategy(from, {
-	// 	output: {
-	// 		filename: toFilename,
-	// 	}
-	// }).output.filename, fromFilename, 'object merged');
-	// t.is(from.output.filename, toFilename, 'from is a dirty object');
-
-	// from = getPristineObject();
-	// t.not(from.output.filename, toFilename, 'from is a pristine object');
-	// t.deepEqual(strategy(from.resolve.modules, ['source', 'bower_components']), ['source', 'bower_components', 'node_modules'])
-	// console.log(from)
-	// t.deepEqual(strategy(undefined, from), from);
+	t.is(strategy(from, {
+		output: {
+			filename: toFilename,
+		}
+	}).output.filename, fromFilename, 'object merged');
+	t.is(from.output.filename, fromFilename, 'from is a pristine object');
+	t.deepEqual(strategy(from.resolve.modules, ['source', 'bower_components']), ['source', 'bower_components', 'node_modules'])
+	t.deepEqual(strategy(undefined, from), from);
 });
