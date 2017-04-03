7ca22fc022b8cd6fbf9be4564d5d8f94e337f5ca
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-02 21:54:40 -0300

    test: dynamic sep

--- test/abs-alias.spec.js
+++ test/abs-alias.spec.js
@@ -1,11 +1,12 @@
 import test from 'ava-spec';
+import { sep } from 'path';
 import { absAlias } from '../tools';
 
 test('tools/absAlias', t => {
 	const src = absAlias(process.cwd());
 	t.is(toString.call(src), '[object Function]');
 	t.is(src(), process.cwd());
-	t.is(src('index.js'), `${process.cwd()}/index.js`);
-	t.is(src('core', 'index.js'), `${process.cwd()}/core/index.js`);
-	t.is(src('core', 'facade', 'index.js'), `${process.cwd()}/core/facade/index.js`);
+	t.is(src('index.js'), `${process.cwd()}${sep}index.js`);
+	t.is(src('core', 'index.js'), `${process.cwd()}${sep}core${sep}index.js`);
+	t.is(src('core', 'facade', 'index.js'), `${process.cwd()}${sep}core${sep}facade${sep}index.js`);
 });
--- test/context-entries.spec.js
+++ test/context-entries.spec.js
@@ -1,30 +1,31 @@
 import test from 'ava-spec';
+import { sep } from 'path';
 import { contextEntries } from '../tools';
 
 test('tools/contextEntries object', t => {
 	const inputEntry = {
 		index: ['foo.js'],
 	};
-	const entry = contextEntries('./context/test/', inputEntry);
+	const entry = contextEntries(`./context/test/`, inputEntry);
 	t.truthy(inputEntry.index[0] === entry.index[0]);
-	t.truthy(entry.index[0].indexOf('../../') === 0);
+	t.truthy(entry.index[0].indexOf(`..${sep}..${sep}`) === 0);
 });
 
 test('tools/contextEntries array', t => {
 	const inputEntry = ['foo.js'];
-	const entry = contextEntries('./context/array/test/', inputEntry);
+	const entry = contextEntries(`./context/array/test/`, inputEntry);
 	t.falsy(inputEntry[0] === entry[0]);
-	t.truthy(entry[0].indexOf('../../../') === 0);
+	t.truthy(entry[0].indexOf(`..${sep}..${sep}..${sep}`) === 0);
 });
 
 test('tools/contextEntries string', t => {
 	const inputEntry = 'foo.js';
-	const entry = contextEntries('./context/string/test/2', inputEntry);
+	const entry = contextEntries(`./context/string/test/2`, inputEntry);
 	t.falsy(inputEntry === entry);
-	t.truthy(entry.indexOf('../../../../') === 0);
+	t.truthy(entry.indexOf(`..${sep}..${sep}..${sep}..${sep}`) === 0);
 });
 
 test('tools/contextEntries unexpected', t => {
-	const entry = contextEntries('./context/string/test/2', undefined);
+	const entry = contextEntries(`./context/string/test/2`, undefined);
 	t.is(entry, undefined);
 });
