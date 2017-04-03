b2be073c216aca6d35ee3e91caa8da6a42687e75
Adrian Miranda <adriancmiranda@gmail.com>
2017-04-03 00:53:50 -0300

    chore: client-watch settings

--- test/configs/client-watch.js
+++ test/configs/client-watch.js
@@ -9,5 +9,17 @@ module.exports = get => clientConfig(get).cfg('entry', [
 	devtool: '#cheap-module-eval-source-map',
 	output: {
 		publicPath: get('watch.publicPath')
+	},
+	devServer: {
+		quiet: true,
+		inline: true,
+		compress: true,
+		historyApiFallback: true,
+		contentBase: get('cwd', get('path.output')),
+		stats: 'errors-only',
+		host: get('watch.host'),
+		port: get('watch.port'),
+		open: get('watch.autoOpenBrowser'),
+		proxy: get('watch.proxyTable')
 	}
 });
