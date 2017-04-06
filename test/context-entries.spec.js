e049e6ee4b93b574f927ad651408f7158874302b
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-04-06 20:47:38 -0300

    fix: context-entries

--- test/context-entries.spec.js
+++ test/context-entries.spec.js
@@ -6,26 +6,28 @@ test('tools/contextEntries object', t => {
 	const inputEntry = {
 		index: ['foo.js'],
 	};
-	const entry = contextEntries(`./context/test/`, inputEntry);
+	const entry = contextEntries('./context/test/', inputEntry);
 	t.truthy(inputEntry.index[0] === entry.index[0]);
-	t.truthy(entry.index[0].indexOf(`..${sep}..${sep}`) === 0);
+	t.is(entry.index[0], './context/test/foo.js');
 });
 
 test('tools/contextEntries array', t => {
 	const inputEntry = ['foo.js'];
-	const entry = contextEntries(`./context/array/test/`, inputEntry);
+	const entry = contextEntries('/context/array/test/', inputEntry);
 	t.falsy(inputEntry[0] === entry[0]);
-	t.truthy(entry[0].indexOf(`..${sep}..${sep}..${sep}`) === 0);
+	t.truthy(entry[0], '/context/array/test/foo.js');
 });
 
 test('tools/contextEntries string', t => {
 	const inputEntry = 'foo.js';
-	const entry = contextEntries(`./context/string/test/2`, inputEntry);
+	const entry = contextEntries('./context/string/test/2', inputEntry);
 	t.falsy(inputEntry === entry);
-	t.truthy(entry.indexOf(`..${sep}..${sep}..${sep}..${sep}`) === 0);
+	t.is(entry, './context/string/test/2/foo.js');
 });
 
 test('tools/contextEntries unexpected', t => {
-	const entry = contextEntries(`./context/string/test/2`, undefined);
-	t.is(entry, undefined);
+	const voidEntry = contextEntries('./context/string/test/2', undefined);
+	const voidContext = contextEntries(undefined, 'foo.js');
+	t.is(voidEntry, undefined);
+	t.is(voidContext, 'foo.js');
 });
