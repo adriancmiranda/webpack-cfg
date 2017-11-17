7c3c497f049ed181b10df903376f920e74caafb6
Adrian Miranda <adriancmiranda@gmail.com>
2017-11-17 08:42:15 -0200

    chore: ...

--- example/@bin/www/app/index.js
+++ example/@bin/www/app/index.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const debug = require('debug')('eguru:app');
+const debug = require('debug');
 const morgan = require('morgan');
 
+const info = debug('eguru:app');
 const app = express();
 
 app.enable('trust proxy');
@@ -14,4 +15,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+info('app: up & running');
 module.exports = app;
