e3fb689cb5c39835b4b3617dce7d037ca819863e
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-11-18 12:52:32 -0200

    chore: add node.module

--- example/@bin/webpack/templates/server-base.js
+++ example/@bin/webpack/templates/server-base.js
@@ -5,30 +5,33 @@ const commonBase = require('./');
 module.exports = $ => commonBase($)
 .cfg('entry', $('path.server'), contextEntries)
 .cfg({
-	name: 'server:base',
-	target: 'async-node',
-	devtool: 'sourcemap',
-	recordsPath: $('cwd', $('path.output.bundle', 'records')),
-	output: {
-		libraryTarget: 'commonjs2',
-	},
-	module: {
-		exprContextCritical: false,
-	},
-	node: Object.assign({
-		global: true,
-		Buffer: true,
-		process: true,
-		console: false,
-		__dirname: true,
-		__filename: true,
-		setImmediate: true,
-	}, $('node')),
-	plugins: [
-		new webpack.BannerPlugin({
-			banner: `try { require('source-map-support').install(); } catch(err) { /* ! */ }`,
-			entryOnly: false,
-			raw: true,
-		}),
-	],
+  name: 'server:base',
+  target: 'async-node',
+  devtool: 'sourcemap',
+  recordsPath: $('cwd', $('path.output.bundle', 'records')),
+  output: {
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    exprContextCritical: false,
+  },
+  node: Object.assign({
+    crypto: 'empty',
+    global: true,
+    process: true,
+    Buffer: true,
+    console: false,
+    module: false,
+    __dirname: true,
+    __filename: true,
+    setImmediate: true,
+    clearImmediate: true,
+  }, $('node')),
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `try { require('source-map-support').install(); } catch(err) { /* ! */ }`,
+      entryOnly: false,
+      raw: true,
+    }),
+  ],
 });
