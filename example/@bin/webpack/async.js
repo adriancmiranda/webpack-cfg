8ea184f0c0f4ad1e1a2c8e649c93e54f344a11a3
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-11-18 11:31:08 -0200

    fix: typo

--- example/@bin/webpack/async.js
+++ example/@bin/webpack/async.js
@@ -1,7 +1,7 @@
 const portfinder = require('portfinder');
 const createConfig = require('./');
 
-// Saddly, Promises only works in directly calls.
+// Sadly, Promises only works in cli calls from webpack (at least apparently).
 module.exports = (argv) => portfinder.getPortPromise().then((port) => {
   process.env.PORT = argv.port = port;
   return createConfig(argv);
