15cd84cff4770846015da190bcba918a27c03cba
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-11-18 12:51:22 -0200

    refactor: views

--- example/app.js
+++ example/app.js
@@ -9,7 +9,7 @@ const morgan = require('morgan');
 const routes = require('./routes');
 
 const info = debug('info:app');
-const viewsDir = path.join(__dirname, 'public');
+const viewsDir = path.join(__dirname, 'views');
 const publicDir = path.join(__dirname, 'public');
 const staticDir = path.join(__dirname, 'static');
 const favicon = path.join(staticDir, 'favicon.ico');
@@ -19,7 +19,7 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', viewsDir);
 app.use(serveFavicon(favicon));
-app.use([express.static(publicDir), express.static(staticDir)]);
+app.use(express.static(publicDir));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
