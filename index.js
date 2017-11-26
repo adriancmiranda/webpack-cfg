8bbffe099f5e2e31789b3e972b2e5574217b3733
Adrian Miranda <adriancmiranda@gmail.com>
2017-11-26 19:32:02 -0200

    chore: dynamic parent filename

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const { is, as } = require('describe-type');
 const WebpackCfg = require('./lib');
 
-module.exports = (pattern, settings) => {
-	return new WebpackCfg(path.parse(module.parent.filename), pattern, settings);
+module.exports = (pattern, settings, parentFilename) => {
+	if (is.string(settings)) {
+		parentFilename = settings;
+		settings = undefined;
+	}
+	return new WebpackCfg(path.parse(as(String, parentFilename) || module.parent.filename), pattern, settings);
 };
