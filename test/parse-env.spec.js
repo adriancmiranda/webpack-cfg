2ec45e2e27629f923446c89c55788fab5f15a5e0
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-06-02 03:13:08 -0300

    fix: parse-env definitions

--- test/parse-env.spec.js
+++ test/parse-env.spec.js
@@ -2,24 +2,24 @@ import test from 'ava-spec';
 import { parseEnv } from '../tools';
 
 test('tools/parseEnv params', t => {
-	t.deepEqual(parseEnv.params(1), {});
-	t.deepEqual(parseEnv.params(null), {});
-	t.deepEqual(parseEnv.params(undefined), {});
-	t.deepEqual(parseEnv.params(), {});
-	t.deepEqual(parseEnv.params('foo'), {});
-	t.deepEqual(parseEnv.params({ env: 'development', run: 'client-watch,server-watch' }), {
+	t.deepEqual(parseEnv(1), {});
+	t.deepEqual(parseEnv(null), {});
+	t.deepEqual(parseEnv(undefined), {});
+	t.deepEqual(parseEnv(), {});
+	t.deepEqual(parseEnv('foo'), {});
+	t.deepEqual(parseEnv({ env: 'development', run: 'client-watch,server-watch' }), {
 		env: 'development',
 		run: ['client-watch', 'server-watch']
 	});
-	t.deepEqual(parseEnv.params({ dev: true, run: 'client-watch,server-watch' }), {
+	t.deepEqual(parseEnv({ dev: true, run: 'client-watch,server-watch' }), {
 		dev: true,
 		run: ['client-watch', 'server-watch']
 	});
-	t.deepEqual(parseEnv.params({ env: 'production,test', run: 'server-watch,client-watch' }), {
+	t.deepEqual(parseEnv({ env: 'production,test', run: 'server-watch,client-watch' }), {
 		env: ['production', 'test'],
 		run: ['server-watch', 'client-watch'],
 	});
-	t.deepEqual(parseEnv.stringify({ env: 'production,test', run: 'server-watch,client-watch' }), {
+	t.deepEqual(parseEnv.definitions({ env: 'production,test', run: 'server-watch,client-watch' }), {
 		env: '"production,test"',
 		run: '"server-watch,client-watch"',
 	});
